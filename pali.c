d4df3e62528c9b493e9f3d322380048b96daf21c
Vinod Sasidharan <vinods@Vinods-MacBook-Air.local>
2017-10-11 22:01:35 +0530

    2nd feature commit

--- pali.c
+++ pali.c
@@ -8,6 +8,7 @@
 //        1.2 - Updated with new version number
 //        1.3 - new version number for test repo:test branch
 //        1.4 - version number of 1st feature commit
+//        1.5 - version number of 2nd feature commit
 // ----------------------------------------
 
 #include <stdio.h>
